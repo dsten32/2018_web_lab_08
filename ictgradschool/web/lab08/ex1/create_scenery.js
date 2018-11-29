bbc72af3ac27b2c1934bd6c7ab6868c156757c8e
Darcy Cowan <dwc1@students.waikato.ac.nz>
2018-11-29 14:06:21 +1300

    did the dolphins disappearing and reappearing on command. I am Aquaman.

--- ictgradschool/web/lab08/ex1/create_scenery.js
+++ ictgradschool/web/lab08/ex1/create_scenery.js
@@ -1,3 +1,47 @@
 $(document).ready(function () {
     // TODO: Your code here
+//     var rButtons = $(':radio');
+// rButtons.change(function () {
+
+// })
+
+
+    $(":radio").on("click", function () {
+        let str = $(this).attr('id');
+        str = str.substring(6);
+        console.log(str);
+        if (str === "background6") {
+            $('#background').attr('src', "../images/" + str + ".gif");
+        } else {
+            $('#background').attr('src', "../images/" + str + ".jpg");
+        }
+    });
+
+
+    $(':checkbox').each(function () {
+
+        let str = $(this).attr('id');
+        str = str.substring(6);
+        console.log("sdc");
+        if ($(this).is(":checked")) {
+            $('#'+str+"").attr('style', "display:block");
+        } else {
+            $('#'+str+"").attr('style', "display:none");
+        }
+    })
+
+
+    $(":checkbox").on("click",function () {
+        // clearDolph();
+        let str = $(this).attr('id');
+        str = str.substring(6);
+        // console.log(str + " attr: " +$(this).is(':checked'));
+        if($(this).is(':checked')){
+            $('#'+str+"").attr('style', "display:block");
+        } else {
+            $('#'+str+"").attr('style', "display:none");
+        }
+    });
+
+
 });
